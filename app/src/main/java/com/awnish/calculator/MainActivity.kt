fab461206e2868ae3758bd0bcc9845bb19580ce4
Awnish <awnishyaav@gmail.com>
2026-09-20 17:44:53 +0530

    Use FragmentActivity for biometric prompt

--- app/src/main/java/com/awnish/calculator/MainActivity.kt
+++ app/src/main/java/com/awnish/calculator/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.awnish.calculator
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Surface
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.fragment.app.FragmentActivity
 import com.awnish.calculator.ui.AwnishApp
 import com.awnish.calculator.ui.theme.AwnishTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
