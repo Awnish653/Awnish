be7285ef723a0ef44fa2cddf499db4fbd38341d5
Awnish <awnishyaav@gmail.com>
2026-09-20 18:35:57 +0530

    Make calculator immersive fullscreen

--- app/src/main/java/com/awnish/calculator/MainActivity.kt
+++ app/src/main/java/com/awnish/calculator/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.awnish.calculator
-
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.fragment.app.FragmentActivity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.fragment.app.FragmentActivity
 import com.awnish.calculator.ui.AwnishApp
 import com.awnish.calculator.ui.theme.AwnishTheme
 
@@ -13,12 +15,22 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        enterImmersiveFullscreen()
         setContent {
             AwnishTheme {
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    AwnishApp(this)
-                }
+                Surface(color = MaterialTheme.colorScheme.background) { AwnishApp(this) }
             }
+        }
+    }
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) enterImmersiveFullscreen()
+    }
+    private fun enterImmersiveFullscreen() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            hide(WindowInsetsCompat.Type.systemBars())
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
 }
