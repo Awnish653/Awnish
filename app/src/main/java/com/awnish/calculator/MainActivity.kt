d99c7b7c65ccced79dca9c817ad9964f68632852
Awnish <awnishyaav@gmail.com>
2026-09-20 21:19:32 +0530

    Match calculator system bars to Samsung-style light UI

--- app/src/main/java/com/awnish/calculator/MainActivity.kt
+++ app/src/main/java/com/awnish/calculator/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.awnish.calculator
+
+import android.graphics.Color as AndroidColor
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.compose.material3.MaterialTheme
@@ -15,22 +17,23 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        enterImmersiveFullscreen()
+        configureSystemBars()
         setContent {
             AwnishTheme {
-                Surface(color = MaterialTheme.colorScheme.background) { AwnishApp(this) }
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    AwnishApp(this)
+                }
             }
         }
     }
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) enterImmersiveFullscreen()
-    }
-    private fun enterImmersiveFullscreen() {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+    private fun configureSystemBars() {
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        window.statusBarColor = AndroidColor.rgb(250, 247, 248)
+        window.navigationBarColor = AndroidColor.rgb(250, 247, 248)
         WindowInsetsControllerCompat(window, window.decorView).apply {
-            hide(WindowInsetsCompat.Type.systemBars())
-            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            isAppearanceLightStatusBars = true
+            isAppearanceLightNavigationBars = true
         }
     }
 }
